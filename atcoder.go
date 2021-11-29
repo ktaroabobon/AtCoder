161b7c5aa4da78af84c400641a4ae82e9533fbb4
ktaroabobon <ktaroabobon@gmail.com>
2021-11-29 13:36:35 +0900

    add: ABC226

--- atcoder.go
+++ atcoder.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"math"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 // page URL:
@@ -15,16 +20,42 @@ import (
 /*
 main関数
 */
+type RuneScanner struct {
+	r   io.Reader
+	buf [16]byte
+}
 
+func NewRuneScanner(r io.Reader) *RuneScanner {
+	return &RuneScanner{r: r}
+}
+
+func (s RuneScanner) Scan() (rune, error) {
+	n, err := s.r.Read(s.buf[:])
+	if err != nil {
+		return 0, err
+	}
+
+	r, size := utf8.DecodeRune(s.buf[:n])
+	if r == utf8.RuneError {
+		return 0, errors.New("RuneError")
+	}
+
+	s.r = io.MultiReader(bytes.NewReader(s.buf[size:n]), s.r)
+	return r, nil
+}
 func main() {
-LOOP:
-	for i := 0; i < 10; i++ {
-		for j := 0; j < 10; j++ {
-			fmt.Printf("i:%d j:%d\n", i, j)
-			if i > 5 && j > 5 {
-				break LOOP
-			}
+	s := NewRuneScanner(strings.NewReader("Hello World!"))
+	for {
+		r, err := s.Scan()
+		if err == io.EOF {
+			break
 		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%c\n", r)
 	}
 }
 
